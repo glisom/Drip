25b9db2da27c56b97f913cd10f67740bec56e82f
Grant Isom <grant.isom@cerner.com>
2018-05-18 09:48:27 -0500

    drip coffee notebook

---
+++ drip/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  drip
+//
+//  Created by Isom,Grant on 5/18/18.
+//  Copyright © 2018 Grant Isom. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
